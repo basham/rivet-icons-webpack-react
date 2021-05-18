c6721b3be22ee57d490df8195e3eb254a982e21c
Chris Basham <chris@bash.am>
2021-05-18 13:44:38 -0400

    Build environment

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import 'react/umd/react.production.min.js?asset'
+import 'react-dom/umd/react-dom.production.min.js?asset'
+import 'rivet-core/css/rivet.min.css?asset'
+import 'rivet-icons/dist/rivet-icons.css?asset'
+import 'rivet-icons/dist/rivet-icons.js?asset'
+import 'rivet-icons/dist/rivet-icon-element.js?asset'
+import './index.html?public'
+
+import React from 'react'
+import { render } from 'react-dom'
+
+function App () {
+  return (
+    <>
+      <rvt-icon name="star" />
+    </>
+  )
+}
+
+render(
+  <App />,
+  document.getElementById('app')
+)
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path')
+
+module.exports = {
+  entry: {
+    app: path.resolve('./src/index.js')
+  },
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM'
+  },
+  output: {
+    filename: `[name].js`,
+    path: path.resolve('./dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        resourceQuery: /asset/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/[base]'
+        }
+      },
+      {
+        resourceQuery: /public/,
+        type: 'asset/resource',
+        generator: {
+          filename: '[base]'
+        }
+      }
+    ]
+  }
+}
