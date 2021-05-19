9b1059371f23c18f68bb898e28306031052fdff6
Chris Basham <chris@bash.am>
2021-05-19 11:51:18 -0400

    Import static icon deps from package

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import 'react/umd/react.production.min.js?asset'
 import 'react-dom/umd/react-dom.production.min.js?asset'
 import 'rivet-core/css/rivet.min.css?asset'
-import '../build/rivet-icons.css?asset'
+import 'rivet-icons/dist/rivet-icon-element.js?asset'
+import 'rivet-icons/dist/rivet-icons.css?asset'
 import '../build/rivet-icons.js?asset'
-import '../build/rivet-icon-element.js?asset'
 import './index.html?root'
 
 import React from 'react'
